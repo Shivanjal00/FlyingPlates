c3beef95efdaaa94ff615eb09e3176bf99cfa395
Shivanjal_walkingtree <shivanjal@walking.tech>
2024-10-09 18:32:34 +0530

    6:32

--- app/src/main/java/com/example/flyingplates/Fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/flyingplates/Fragment/HistoryFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.flyingplates.R
+import com.example.flyingplates.adapter.BuyAgainAdapter
+import com.example.flyingplates.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
+
+    private lateinit var binding : FragmentHistoryBinding
+    private lateinit var buyAgainAdapter : BuyAgainAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +24,18 @@ class HistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(layoutInflater,container,false)
+        setupRecyclerView()
+        return binding.root
+    }
+
+    private fun setupRecyclerView(){
+        val buyAgainFoodName = arrayListOf("Food", "Food1", "Food2", "Food3", "Food4")
+        val buyAgainFoodPrice = arrayListOf("$5", "$10", "$20", "$30", "$40")
+        val buyAgainFoodImage = arrayListOf(R.drawable.pancack,R.drawable.greennoodle,R.drawable.fruitsalad,R.drawable.pancack,R.drawable.greennoodle)
+        buyAgainAdapter = BuyAgainAdapter(buyAgainFoodName, buyAgainFoodPrice, buyAgainFoodImage)
+        binding.BuyAgainRecyclerView.adapter = buyAgainAdapter
+        binding.BuyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
     companion object {
